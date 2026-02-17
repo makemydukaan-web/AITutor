d04ff3e9446bb8f64c0cf88dfb0dbf008142918f
makemydukaan-web <makemydukaan@gmail.com>
2026-02-17 13:21:46 +0530

    Update index.ts

--- frontend/src/server/index.ts
+++ frontend/src/server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import registerRoute from "./routes/auth/register";
+import registerRoute from "./routes/auth/register.ts";
 
 const app = express();
 app.use(cors());
